a192e1c72668165aa913d882ca67f7a07fe01924
ahmed811s <163235612+ahmed811s@users.noreply.github.com>
2025-02-11 19:11:05 +0200

    Update Logo.jsx

--- src/components/1-Header/Logo.jsx
+++ src/components/1-Header/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img src=url"/public/r1.webp" alt="Företagslogotyp" className="logo-img" loading="lazy" />
+      <img src="/public/r1.webp" alt="Företagslogotyp" className="logo-img" loading="lazy" />
     </div>
   );
 };
